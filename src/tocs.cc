d2e6928a28d630ff72d3fee04ff3efaccc6adc19
Chris B <chrismjboyle@gmail.com>
2017-12-11 00:29:46 +0000

    Various Updates
    -> Extended status bar
    -> Added input parsing
    -> General tidying up

--- src/tocs.cc
+++ src/tocs.cc
@@ -38,7 +38,9 @@ strvec m_sitreps;
 strvec maps;
 strvec names;
 str pr_str = "  >";
+str pl_la = " -- ";
 bool verbose = false;
+bool friendly_fire = false;
 
 //************************************************************************//
 //** main "header" *******************************************************//
@@ -57,6 +59,15 @@ void read_arsenal(str);
 void read_names(str);
 int game_status();
 void display();
+void sort_input(int);
+int move(Player*, int);
+int kill_with(Player*, int);
+int equip(Player*, int);
+void camp(Player*);
+void hide(Player*);
+void buy();
+void ai_buy(Player*, int, int);
+int equipped_ammo_left(Player*);
 
 namespace po = boost::program_options;
 
@@ -148,7 +159,7 @@ void game_loop(){
     // display current cell
     display();
     // get user input
-    get_clean_input(pr_str, 2);
+    sort_input(get_clean_input(pr_str, 2));
   }
 
   switch (status) {
@@ -178,11 +189,13 @@ void game_loop(){
 //************************************************************************//
 //************************************************************************//
 void display(){
+  std::cout << "  -------------------------------------------------------\n\n";
   std::cout << "  [" << game_map.timer << "s to go]"
 	    << "  [HP:" << players[0]->HP << "]"
 	    << "  [" << players[0]->Equipped->Name
-	    << " : " << players[0]->Equipped->AmmoLeft
-	    << " / " << players[0]->Equipped->AmmoMax << "]\n\n"
+	    << " : " << equipped_ammo_left(players[0])
+	    << " / " << players[0]->Equipped->AmmoMax << "]"
+	    << "  [" << pl_la << "]\n\n"
 	    << "  " << players[0]->Position->Description << "\n\n";
   int plc = 0, i = 0, pltc = 0;
   str inb = "";
@@ -221,6 +234,21 @@ void display(){
     }
   }
   std::cout << " " << get_rand_sitrep(plc) << "\n\n  What would you like to do?\n" << std::endl;
+}
+
+//************************************************************************//
+//************************************************************************//
+int equipped_ammo_left(Player* p){
+  int i = 0;
+  for (auto w: global_arsenal){
+    if (w == p->Equipped){
+      return p->Ammobelt[i];
+    }
+    i += 1;
+  }
+  // TODO error out here
+  assert(0);
+  return 0;
 }
 
 //************************************************************************//
@@ -289,10 +317,11 @@ void quick_setup(){
   if (verbose){std::cout << "  setting up players" << std::endl;}
   for (int i = 0; i < nop; i++){
     p = new Player;
+    p->Ammobelt.assign(20, 0);
     p->isCT = oe_team;
     p->Name = names[randint(int(names.size()), 0)];
-    std::cout << global_arsenal[0]->Name << std::endl;
     p->Arsenal.push_back(&(*global_arsenal[0]));
+    p->Ammobelt[0] = global_arsenal[0]->AmmoMax;
     p->Equipped = p->Arsenal[0];
     if (oe_team){
       p->Position = &game_map.Cells[cts];
@@ -305,9 +334,11 @@ void quick_setup(){
   }
   for (int i = 0; i < nop; i++){
     p = new Player;
+    p->Ammobelt.assign(20, 0);
     p->isCT = !oe_team;
     p->Name = names[randint(names.size(), 0)];
     p->Arsenal.push_back(&(*global_arsenal[0]));
+    p->Ammobelt[0] = global_arsenal[0]->AmmoMax;
     p->Equipped = p->Arsenal[0];
     if (!oe_team){
       p->Position = &game_map.Cells[cts];
@@ -428,6 +459,175 @@ void get_maps(std::string map_dir, strvec &maps){
     maps.push_back(i->path().string());
     if (verbose){std::cout << "    > found map \"" << i->path().string() << "\"" << std::endl;}
   }
+}
+
+//************************************************************************//
+//************************************************************************//
+void sort_input(int retv){
+  if (retv > 0 && retv <= 8){
+    if (move(players[0], retv - 1)){
+      pl_la = " moved ";
+      game_map.timer -= 2;
+    }
+    else{
+      pl_la = " cannot move there! ";
+    }
+  }
+  else if (retv > 8 && retv <= 29){
+    int res = kill_with(players[0], retv - 9);
+
+    if (res == 2){
+      pl_la = " you shot accurately ";
+      game_map.timer -= 1;
+    }
+    else if (res == 1){
+      pl_la = " you shot ";
+      game_map.timer -= 1;
+    }
+    else if (res == -1){
+      pl_la = " you don't have that ";
+    }
+    else{
+      pl_la = " you missed ";
+      game_map.timer -= 1;
+    }
+  }
+  else if (retv > 29 && retv <= 50){
+    pl_la = " e ";
+    equip(players[0], retv - 30);
+  }
+  else if (retv == 50){
+    pl_la = " h ";
+    hide(players[0]);
+    game_map.timer -= 3;
+  }
+  else if (retv == 51){
+    pl_la = " c ";
+    camp(players[0]);
+  }
+  else if (retv == 52){
+    pl_la = " b ";
+    buy();
+  }
+}
+
+//************************************************************************//
+//************************************************************************//
+Cell* find_cell(int id){
+  int i = 0;
+  for (std::vector<Cell>::iterator it = game_map.Cells.begin(); it != game_map.Cells.end(); it++){
+    if (game_map.Cells[i].ID == id){
+      return &game_map.Cells[i];
+    }
+    i += 1;
+  }
+  // TODO: ERROR OUT HERE
+  assert(0);
+  return &game_map.Cells[0];
+}
+
+//************************************************************************//
+//************************************************************************//
+int move(Player* p, int direction){
+  int dest_id = p->Position->Directions[direction];
+  if (dest_id > 0){
+    p->Position = find_cell(dest_id);
+    return 1;
+  }
+  else {
+    return -1;
+  }
+}
+
+//************************************************************************//
+//************************************************************************//
+int kill_with(Player* p, int weapon){
+  std::vector<Player*> pop;
+  Player* tohit;
+  int popc = 0;
+  for (auto op: players){
+    if (op->Position == p->Position){
+      if (friendly_fire || (p->isCT != op->isCT)){
+	pop.push_back(op);
+	popc += 1;
+      }
+    }
+  }
+  tohit = pop[randint(popc, 0)];
+
+  bool playerhas = false;
+  for (auto w: p->Arsenal){
+    if (w == global_arsenal[weapon]){
+      playerhas = true;
+    }
+  }
+
+  if (!playerhas){
+    return -1;
+  }
+
+  int damage = global_arsenal[weapon]->Damage + randint(20, 0) - 10;
+  double hits = 0.0, rof, hitp;
+  rof = (double)global_arsenal[weapon]->ROF;
+
+  for (int i = 0; i < rof; i++){
+    if (randint(100, 0) < (int)((double)(global_arsenal[weapon]->Accuracy) * p->AccMod_out * tohit->AccMod_in)){
+      tohit->HP -= damage;
+      hits += 1.0;
+    }
+  }
+
+  hitp = hits / rof;
+  
+  if (hitp == 0.0){
+    return -2;
+  }
+  else if ((hitp > 0.0) && (hitp < 1.0)){
+    return 1;
+  }
+  else{
+    return 2;
+  }
+}
+
+//************************************************************************//
+//************************************************************************//
+int equip(Player* p, int weapon){
+  bool playerhas = false;
+  for (auto w: p->Arsenal){
+    if (w == global_arsenal[weapon]){
+      playerhas = true;
+    }
+  }
+
+  if (!playerhas){
+    return -1;
+  }
+
+  p->Equipped = global_arsenal[weapon];
+}
+
+//************************************************************************//
+//************************************************************************//
+void hide(Player* p){
+  p->AccMod_in = 0.8;
+}
+
+
+//************************************************************************//
+//************************************************************************//
+void camp(Player* p){
+  p->AccMod_in = 1.1;
+}
+
+//************************************************************************//
+//************************************************************************//
+void buy(){
+}
+
+//************************************************************************//
+//************************************************************************//
+void ai_buy(Player* p, int index, int type){
 }
 
 //************************************************************************//
